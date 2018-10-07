f37d2ab1052b8833531b3f363812276d6237128a
S1ealth <s1ealth@me.com>
2018-10-07 06:21:46 +0300

    изменит конфиги, добавил css добавит шаблонизатор карт добавит layout для карт, сетки. (mobail) добавит иконки (не все)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,56 @@
 import './style.css';
+import Events from './events.json';
+import Stats from './stats.svg';
+
 console.log('hello there');
-// async function foo() {
-//   let bar = await fetch();
-// }
+console.log(Events.events);
+
+window.addEventListener('load', () => {
+  Events.events.forEach((element) => {
+    createCard(element);
+  });
+});
+function createCard(e) {
+  // Test to see if the browser supports the HTML template element by checking
+// for the presence of the template element's content attribute.
+  if ('content' in document.createElement('template')) {
+    let card = document.getElementById('card-tempo');
+    let clone = document.importNode(card.content, true);
+
+    let cloneCard = clone.querySelector('.card');
+    //  title
+    let title = clone.querySelector('.title');
+    title.innerHTML = e.title;
+    // icon
+    let icon = clone.querySelector('.icon');
+    switch (e.icon) {
+      case 'stats':
+        icon.src = Stats;
+        break;
+      default:
+        icon.src = '';
+    }
+    //  description
+    let desc = clone.querySelector('.description');
+    if (e.description === null) {
+      desc.remove();
+    } else {
+      desc.innerHTML = e.description;
+    }
+    //  source
+    let source = clone.querySelector('.source');
+    source.innerHTML = e.source;
+    //  time
+    let time = clone.querySelector('.time');
+    time.innerHTML = e.time;
+    let cardGrid = document.getElementById('card-grid');
+    console.log(e);
+    if (e.size === 's') {
+      cloneCard.classList.add('card_s');
+    }
+    cardGrid.appendChild(clone);
+  } else {
+    // Find another way to add the rows to the table because
+  // the HTML template element is not supported.
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,27 +4,27 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   devServer: {
-    contentBase: './dist'
+    contentBase: './dist',
   },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            'file-loader'
-          ]
-        }
-      ]
-  }
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
