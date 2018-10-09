0270031692a44b44822cd197df2aa28793cd9673
S1ealth <s1ealth@me.com>
2018-10-09 18:14:46 +0300

    добавит набросок секти с камерами и изменит стили хэда\фута +приберет в css +добавит первый вариант сетки для камер (думаю что показывать через области будет ок) + рефактор футера\хэдера

--- src/surv.js
+++ src/surv.js
@@ -1,1 +1,9 @@
+import './style.css';
+import Hls from 'hls.js';
 console.log('im watching you');
+window.addEventListener('load', (e)=> {
+  if (Hls.isSupported()) {
+    console.log('hello, hls.js (inside node)!');
+  }
+})
+;
