18ea1128d479549194f9835dcec5bbd4a70fb8f9
S1ealth <s1ealth@me.com>
2018-10-12 03:09:23 +0300

    зарефакторит проверку на тип событий. +для динамического управления перетащил их в отдельный массив. будет работать потому что если такого типа нет в массиве => будет undefined и это вывалиться в бэд реквест.

--- apiServer/events/eventsRouter.js
+++ apiServer/events/eventsRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = new express.Router();
 const bodyParser = require('body-parser');
 const events = require('./events');
+// possible types of events.
+const types = ['info', 'critical'];
 //  deal with cors requests
 router.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -19,7 +21,10 @@ router.post('/', (req, res) => {
     limit = req.body.limit;
   }
   if (req.body.hasOwnProperty('type')) {
-    if (req.body.type === 'info' || req.body.type === 'critical') {
+    let type = types.find((type) => {
+      return type === req.body.type;
+    });
+    if (req.body.type === type) {
       events.getEvents(req.body.type, limit)
           .then((data) => {
             res.status(200).json(data);
