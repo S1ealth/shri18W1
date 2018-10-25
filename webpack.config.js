158b5eee705353713ffd1113536e9345c6ef6507
S1ealth <s1ealth@me.com>
2018-10-25 13:14:44 +0300

    добавит ts-конфиги и сделает базовую миграцию

--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,32 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: {
-    index: './src/index.js',
-    surv: './src/surv.js',
+    index: './src/index.ts',
+    surv: './src/surv.ts',
   },
   devServer: {
     contentBase: './dist',
   },
+  devtool: 'source-map',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
+  },
   module: {
+    loaders: [
+      {test: /\.tsx?$/, loader: 'awesome-typescript-loader'},
+    ],
+
+    preLoaders: [
+      {test: /\.js$/, loader: 'source-map-loader'},
+    ],
     rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ],
-      },
+      {test: /\.css$/, use: ['style-loader', 'css-loader']},
+      {test: /\.(png|svg|jpg|gif)$/, use: ['file-loader']},
     ],
   },
 };
