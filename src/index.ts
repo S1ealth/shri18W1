3bd47d3091750bb0951691ef577d348001ea22e1
S1ealth <s1ealth@me.com>
2018-10-25 18:27:14 +0300

    добавит файл с кастомными определениями +еще порция кодрефакторинга (основываясь на линтерах)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-require('./style.css');
-const Stats =  require('./images/stats.svg');
-const Key = require( './images/key.svg');
-const Robot = require( './images/robot-cleaner.svg');
-const Router = require( './images/router.svg');
-const Thermal = require( './images/thermal.svg');
-const Ac = require( './images/air-conditioner.svg');
-const Music = require( './images/music.svg');
-const Fridge = require( './images/fridge.svg');
-const Battery = require( './images/battery.svg');
-const Kettle = require( './images/kettle.svg');
+import Ac from './images/air-conditioner.svg';
+import Battery from './images/battery.svg';
+import Fridge from './images/fridge.svg';
+import Kettle from './images/kettle.svg';
+import Key from './images/key.svg';
+import Music from './images/music.svg';
+import Robot from './images/robot-cleaner.svg';
+import Router from './images/router.svg';
+import Stats from './images/stats.svg';
+import Thermal from './images/thermal.svg';
+import './style.css';
 
 window.addEventListener('load', () => {
   fetchEvents().then((events) => {
@@ -22,10 +22,10 @@ window.addEventListener('load', () => {
   });
 });
 async function fetchEvents(type?: string, limit?: number) {
-  let myHeaders = new Headers();
-  let endpoint:string = 'http://localhost:8000/api/events';
+  const myHeaders = new Headers();
+  const endpoint: string = 'http://localhost:8000/api/events';
   myHeaders.append('x-requested-with', 'XMLHttpRequest');
-  let params = new URLSearchParams();
+  const params = new URLSearchParams();
   if (type) {
     params.append('type', type);
   }
@@ -33,37 +33,35 @@ async function fetchEvents(type?: string, limit?: number) {
     params.append('limit', limit.toString());
   }
   try {
-    let result = await fetch(endpoint,
+    const result = await fetch(endpoint,
         {
-          method: 'POST',
           body: params,
           headers: myHeaders,
+          method: 'POST',
         });
     if (result.status !== 200) {
       throw new Error(`failed with code ${result.status}`);
     } else {
-      let data = await result.json();
+      const data = await result.json();
       return data;
     }
   } catch (e) {
-    console.log(e);
     throw e;
   }
 }
-function createCard(e:object) {
-  console.log(e);
+function createCard(e: object) {
   // Test to see if the browser supports the HTML template element by checking
 // for the presence of the template element's content attribute.
   if ('content' in document.createElement('template')) {
-    let card = (<HTMLInputElement>document.getElementById('card-tempo'));
-    let clone = document.constNode(card.content, true);
+    const card = ( document.getElementById('card-tempo') as HTMLInputElement);
+    const clone = document.constNode(card.content, true);
 
-    let cloneCard = clone.querySelector('.card');
+    const cloneCard = clone.querySelector('.card');
     //  title
-    let title = clone.querySelector('.title').
+    const title = clone.querySelector('.title').
     title.innerHTML = e.title;
     // icon
-    let icon = clone.querySelector('.icon');
+    const icon = clone.querySelector('.icon');
     switch (e.icon) {
       case 'stats':
         icon.src = Stats;
@@ -99,19 +97,19 @@ function createCard(e:object) {
         icon.src = '';
     }
     //  description
-    let desc = clone.querySelector('.description');
+    const desc = clone.querySelector('.description');
     if (e.description === null) {
       desc.remove();
     } else {
       desc.innerHTML = e.description;
     }
     //  source
-    let source = clone.querySelector('.source');
+    const source = clone.querySelector('.source');
     source.innerHTML = e.source;
     //  time
-    let time = clone.querySelector('.time');
+    const time = clone.querySelector('.time');
     time.innerHTML = e.time;
-    let cardGrid = document.getElementById('card-grid');
+    const cardGrid = document.getElementById('card-grid');
     if (e.size === 's') {
       cloneCard.classList.add('card_s');
     }
--- src/surv.ts
+++ src/surv.ts
@@ -1,41 +1,38 @@
-import './style.css';
 import Hls from 'hls.js';
+import './style.css';
 
-let cameras = [
+const cameras = [
   {
+    canvas: 'canv-1',
     container: 'camera-1',
     src: 'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8',
-    canvas: 'canv-1',
   },
   {
+    canvas: 'canv-2',
     container: 'camera-2',
     src: 'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8',
-    canvas: 'canv-2',
   },
   {
+    canvas: 'canv-3',
     container: 'camera-3',
     src: 'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8',
-    canvas: 'canv-3',
   },
   {
+    canvas: 'canv-4',
     container: 'camera-4',
     src: 'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8',
-    canvas: 'canv-4',
   },
 ];
-console.log('im watching you');
-window.addEventListener('load', (e)=> {
+window.addEventListener('load', (e) => {
   if (Hls.isSupported()) {
-    console.log('hello, hls.js (inside node)!');
     cameras.forEach((camera) => {
-      let {container, src, canvas} = camera;
-      console.log(camera);
+      const {container, src, canvas} = camera;
       getVideo(container, src);
       createCanvas(canvas, container);
     });
   }
 });
-function createCanvas(cId, vId) {
+function createCanvas(cId: string, vId: string) {
   const canvas = document.getElementById(cId);
   canvas.classList.add(vId);
   const ctx = canvas.getContext('2d');
@@ -47,35 +44,26 @@ function createCanvas(cId, vId) {
   }
   loop();
 }
-function getVideo(container, src) {
-  let video = document.getElementById(container);
+function getVideo(container: string, src: string) {
+  const video = document.querySelector(container);
   video.classList.add('hidden');
   if (Hls.isSupported()) {
-    console.log('hls required');
-    let hls = new Hls();
+    const hls = new Hls();
     hls.loadSource(src);
     // bind them together
     hls.attachMedia(video);
     // MEDIA_ATTACHED event is fired by hls object once MediaSource is ready
-    hls.on(Hls.MEDIA_ATTACHED, function() {
-      console.log('video and hls.js are now bound together !');
-    });
-    hls.on(Hls.Events.MANIFEST_PARSED, function(event, data) {
-      console.log('loaded. found ' + data.levels.length + ' quality level');
+    hls.on(Hls.Events.MANIFEST_PARSED, (event, data) => {
       video.play();
-      console.log(hls.currentLevel);
-      console.log(hls.levels);
     });
-    hls.on(Hls.Events.ERROR, function(event, data) {
+    hls.on(Hls.Events.ERROR, (event, data) => {
       if (data.fatal) {
         switch (data.type) {
           case Hls.ErrorTypes.NETWORK_ERROR:
             // try to recover network error
-            console.log('fatal network error encountered, try to recover');
             hls.startLoad();
             break;
           case Hls.ErrorTypes.MEDIA_ERROR:
-            console.log('fatal media error encountered, try to recover');
             hls.recoverMediaError();
             break;
           default:
@@ -88,7 +76,7 @@ function getVideo(container, src) {
   } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
     confirm.log('can play native');
     video.src = src;
-    video.addEventListener('loadedmetadata', function() {
+    video.addEventListener('loadedmetadata', () => {
       video.play();
     });
   }
